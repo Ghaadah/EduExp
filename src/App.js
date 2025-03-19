de0900debf79c23a93047b52ac1483326580c975
Raghad Al Musawi <raghad3@pdx.edu>
2025-03-19 12:35:50 -0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 /* eslint-disable react/jsx-filename-extension */
-import "./style.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './style.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import NavMenu from "./components/Navbar";
-import Home from "./pages/Home";
-import APIData from "./pages/SearchByState";
-import Dashboard from "./pages/Dashboard";
-import SearchPage from "./pages/SearchPage";
-import GraphPage from "./pages/GraphPage";
-import CollegePage from "./pages/CollegePage";
+import NavMenu from './components/Navbar';
+import Home from './pages/Home';
+import APIData from './pages/SearchByState';
+import Dashboard from './pages/Dashboard';
+import SearchPage from './pages/SearchPage';
+import GraphPage from './pages/GraphPage';
+import CollegePage from './pages/CollegePage';
 
 function App() {
   return (
     <Router>
       <NavMenu />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/home" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/searchByState" element={<APIData />} />
@@ -24,7 +24,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/college/:name" element={<CollegePage />} />
         <Route path="/college/:id/:name" element={<CollegePage />} />
-        <Route path="*" element={<div>404: Page Not Found</div>} />
+        {/* <Route path="*" element={<div>404: Page Not Found</div>} /> */}
       </Routes>
     </Router>
   );
